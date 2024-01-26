02aecbadf49fd1f250f6986c2db00f1aea355a19
Timmy <hatem.bassah@gmail.com>
2024-01-26 17:57:00 +0300

    Bug Fixes
    Fixed some bugs that prevented the user from resetting the game as well as some other relevant bugs as well.

---
+++ JS/script.js
@@ -0,0 +1,99 @@
+let data;
+
+window.onload = async function () {
+  const response = await fetch("../word.json");
+
+  if (!response.ok) {
+    throw new Error(`HTTP error! Status: ${response.status}`);
+  }
+
+  data = await response.json();
+  displayWord();
+};
+
+let currentIndex = 0;
+let shuffledWords = [];
+
+document
+  .getElementById("userInput")
+  .addEventListener("keyup", function (event) {
+    if (event.key === "Enter") {
+      checkReading();
+    }
+  });
+
+async function shuffleArray(array) {
+  const shuffledArray = array.slice();
+  for (let i = shuffledArray.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [shuffledArray[i], shuffledArray[j]] = [shuffledArray[j], shuffledArray[i]];
+  }
+  return shuffledArray;
+}
+
+async function displayWord() {
+  document.getElementById("level").innerHTML = currentIndex + 1;
+
+  if (currentIndex === 0) {
+    shuffledWords = await shuffleArray(data.words);
+  }
+
+  const currentWord = shuffledWords[currentIndex];
+
+  const japaneseWordElement = document.getElementById("japanese-word");
+  japaneseWordElement.innerHTML = '';
+
+  const jishoLink = document.createElement('a');
+  jishoLink.href = `https://jisho.org/search/${currentWord.kanji}`;
+  jishoLink.textContent = currentWord.kanji;
+
+  japaneseWordElement.appendChild(jishoLink);
+}
+
+async function checkReading() {
+  const currentWord = shuffledWords[currentIndex];
+  const userInput = document.getElementById("userInput");
+  const userInputValue = userInput.value.trim();
+
+  if (
+    userInputValue === currentWord.reading ||
+    userInputValue === currentWord.romaji
+  ) {
+    if (currentIndex < shuffledWords.length - 1) {
+      userInput.style.boxShadow = "";
+      userInput.style.borderColor = "";
+      currentIndex++;
+    } else {
+      document.getElementById("japanese-word").textContent = "完了";
+      document.getElementById("search-container").innerHTML = `
+              <button id="resetButton" onclick="resetGame()">Reset</button>
+          `;
+      document.getElementById("resetButton").style.display = "block";
+      userInput.style.display = "none";
+      return;
+    }
+  } else {
+    userInput.style.boxShadow = "0 0 10px #fb2577";
+    userInput.style.borderColor = "#fb2577";
+  }
+  userInput.value = "";
+
+  displayWord();
+}
+
+async function resetGame() {
+  currentIndex = 0;
+  await displayWord();
+
+  const resetButton = document.getElementById("resetButton");
+  if (resetButton) {
+    resetButton.style.display = "none";
+  }
+
+  document.getElementById("search-container").innerHTML = `
+    <input type="text" id="userInput" placeholder="">
+    <button onclick="checkReading()">提出</button>
+    <button id="resetButton" style="display:none; text-align: center;" onclick="resetGame()">Reset</button>
+  `;
+}
+
